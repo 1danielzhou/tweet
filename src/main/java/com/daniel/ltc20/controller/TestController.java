69a5fe11fc94a86b6c9c0f20d9f3f70f8e62c587
周成龙 <1907667150@qq.com>
2023-06-22 11:00:21 +0800

    删除无用的代码

--- src/main/java/com/daniel/ltc20/controller/TestController.java
+++ src/main/java/com/daniel/ltc20/controller/TestController.java
@@ -1,14 +1,11 @@
 package com.daniel.ltc20.controller;
 
-import com.daniel.ltc20.domain.InscriptionMintInfo;
 import com.daniel.ltc20.domain.TweetAccount;
-import com.daniel.ltc20.service.InscriptionMintInfoService;
 import com.daniel.ltc20.service.TweetAccountService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,9 +13,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/test")
-public class InscriptionMintInfoController {
-    @Autowired
-    private InscriptionMintInfoService inscriptionMintInfoService;
+public class TestController {
     @Autowired
     private TweetAccountService tweetAccountService;
 
@@ -30,16 +25,5 @@ public class InscriptionMintInfoController {
         }
         System.out.println("----------");
         System.out.println(tweetAccountService.getRandomTweetAccount());
-    }
-
-    @RequestMapping(value = "/queryByAddress", method = RequestMethod.GET)
-    public List<InscriptionMintInfo> queryByAddress(@RequestParam("address") String address) {
-        return inscriptionMintInfoService.queryByAddress(address);
-    }
-
-    @RequestMapping(value = "/queryById", method = RequestMethod.GET)
-    public InscriptionMintInfo queryById(@RequestParam("id") int id) {
-        InscriptionMintInfo inscriptionMintInfo = inscriptionMintInfoService.queryById(id);
-        return inscriptionMintInfo;
     }
 }
